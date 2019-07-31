5414084573fdd7847b0adb38f4893ded5dfe9f01
Markku Uttula <markku.uttula@cleverest.eu>
2019-07-31 22:16:37 +0300

    Tests for “supported platforms”
    Need to mock up a lot of things for this

--- test/index-supported-platform.test.js
+++ test/index-supported-platform.test.js
@@ -3,12 +3,10 @@
 const detectMobileOSK = require('../src/index.js');
 
 describe('on a supported (faked during testing) platform', () => {
-    test('doesn\'t even try to do anything', () => {
-        expect((window || {}).oskDetectionEnabled).not.toBeDefined();
-        // TODO: It actually should try to do something :)
-    });
     test('exports absolutely nothing', () => {
         expect(detectMobileOSK).not.toBeDefined();
-        // TODO: It actually should try to export something?
+    });
+    test('tries to do something', () => {
+        expect((window || {}).oskDetectionEnabled).toBeDefined();
     });
 });
